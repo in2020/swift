4226cf5a209ed50c80a473aa4a68754593fab23e
김인홍 <in20200212@gmail.com>
2018-07-14 23:40:23 +0900

    init

---
+++ listview/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  listview
+//
+//  Created by 김인홍 on 2018. 7. 4..
+//  Copyright © 2018년 hongs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var centerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
